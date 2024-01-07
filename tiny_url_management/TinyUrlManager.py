ba558782a5a2b567cfec71af2a1b1d46336291ec
Suja Shaj <sujashaj98@gmail.com>
2024-01-06 16:30:15 -0800

    Added tiny url manager

---
+++ tiny_url_management/TinyUrlManager.py
@@ -0,0 +1,16 @@
+class TinyUrlManager:
+    def __init__(self, redis_dao, mongo_dao):
+        self.redis_dao = redis_dao
+        self.mongo_dao = mongo_dao
+
+    def get_full_url(self, short_url):
+        full_url = self.redis_client.get(short_url)
+
+        if not full_url:
+            full_url = self.service.mongo_dao.get_url(short_url)
+
+            if not full_url:
+                return None
+
+            self.redis_dao.set_url(short_url, full_url)
+            return full_url
