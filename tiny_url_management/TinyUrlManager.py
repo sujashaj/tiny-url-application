64236cbf0bb6aac57bb527e6817cd35b35280e27
Suja Shaj <sujashaj98@gmail.com>
2024-01-08 12:33:44 -0800

    Fixed naming issues with tiny url manager

--- tiny_url_management/TinyUrlManager.py
+++ tiny_url_management/TinyUrlManager.py
@@ -1,16 +1,16 @@
 class TinyUrlManager:
-    def __init__(self, redis_dao, mongo_dao):
-        self.redis_dao = redis_dao
-        self.mongo_dao = mongo_dao
+    def __init__(self, redisDao, mongoDao):
+        self.redisDao = redisDao
+        self.mongoDao = mongoDao
 
-    def get_full_url(self, short_url):
-        full_url = self.redis_client.get(short_url)
+    def get_url(self, short_url):
+        full_url = self.redisDao.get_url(short_url)
 
         if not full_url:
-            full_url = self.service.mongo_dao.get_url(short_url)
+            full_url = self.mongoDao.get_url(short_url)
 
             if not full_url:
                 return None
 
-            self.redis_dao.set_url(short_url, full_url)
+            self.redisDao.set_url(short_url, full_url)
             return full_url
