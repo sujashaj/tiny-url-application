af76025388fb6be50b45c08f95e9a01535bfb947
Suja Shaj <sujashaj98@gmail.com>
2024-03-03 09:26:37 -0800

    Added route to generate tiny url for the given full url

--- routes/Routes.py
+++ routes/Routes.py
@@ -7,6 +7,7 @@ from tiny_url_management.TinyUrlManager import TinyUrlManager
 class Routes:
     def __init__(self):
         self.bp = Blueprint("routes", __name__)
+        self.host = "http://localhost:8080/"
         self.register_routes()
         self.tiny_url_manager = TinyUrlManager(RedisDao(), MongoDao())
 
@@ -20,6 +21,11 @@ class Routes:
         else:
             abort(404)
 
+    def generate_tiny_url(self, full_url):
+        key = self.tiny_url_manager.generate_key(full_url)
+        return {'tiny_url': self.host + key}, 200
+
     def register_routes(self):
         self.bp.add_url_rule("/", "home", self.home)
         self.bp.add_url_rule("/<short_url>", "redirect_to_full_url", self.redirect_to_full_url, methods=["GET"])
+        self.bp.add_url_rule("/generate_tiny_url", "generate_tiny_url", self.redirect_to_full_url, methods=["POST"])
--- tiny_url_management/TinyUrlManager.py
+++ tiny_url_management/TinyUrlManager.py
@@ -1,7 +1,22 @@
+import random
+import string
+
+
 class TinyUrlManager:
     def __init__(self, redis_dao, mongo_dao):
         self.redis_dao = redis_dao
         self.mongo_dao = mongo_dao
+
+    def generate_random_key(self):
+        characters = string.ascii_uppercase + string.digits
+        return ''.join(random.choice(characters) for _ in range(self.key_length))
+
+    def generate_key(self, full_url):
+        key_candidate = self.generate_random_key()
+        while self.mongo_dao.get_url(key_candidate):
+            key_candidate = self.generate_random_key()
+        self.mongo_dao.set_url(key_candidate, full_url)
+        return key_candidate
 
     def get_url(self, short_url):
         full_url = self.redis_dao.get_url(short_url)
