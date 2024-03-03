7e6deab93b6bdd185c50ddd708e8773867d69a14
Suja Shaj <sujashaj98@gmail.com>
2024-03-03 13:13:22 -0800

    Move backend origin url to env variable

--- routes/Routes.py
+++ routes/Routes.py
@@ -1,13 +1,16 @@
+import os
+
 from flask import Blueprint, request, redirect, abort
 from dao.MongoDao import MongoDao
 from dao.RedisDao import RedisDao
 from tiny_url_management.TinyUrlManager import TinyUrlManager
 
+BACKEND_ORIGIN = os.getenv('BACKEND_ORIGIN')
+
 
 class Routes:
     def __init__(self):
         self.bp = Blueprint("routes", __name__)
-        self.host = "http://localhost:8080/"
         self.register_routes()
         self.tiny_url_manager = TinyUrlManager(RedisDao(), MongoDao())
 
@@ -28,7 +31,7 @@ class Routes:
         key = self.tiny_url_manager.get_short_url(full_url)
         if not key:
             key = self.tiny_url_manager.generate_key(full_url)
-        return {'short_url': self.host + key}, 200
+        return {'short_url': BACKEND_ORIGIN + key}, 200
 
     def register_routes(self):
         self.bp.add_url_rule("/", "home", self.home)
